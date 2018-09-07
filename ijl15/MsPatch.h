d7d3ce67ae93dc6d0e3a88be07e87bf96fd5c356
reanox <reanox@hotmail.com.tw>
2018-09-07 14:50:44 +0800

    修正破攻擊(v113、v120)

--- ijl15/MsPatch.h
+++ ijl15/MsPatch.h
@@ -3,11 +3,11 @@
 
 void No_Cap() {
 	#if MsVer == 113
-	DWORD No_CapAddr = 0x00B1CE26;
-	*(double*)No_CapAddr = 300000.0;
+		DWORD No_CapAddr = 0x00B1CE26;
+		*(double*)No_CapAddr = 300000.0;
 	#elif MsVer == 120
-	double *DamageCap = new double(300000.00);
-	DWORD v120No_CapAddr1 = 0x00867A6C;
-	*(DWORD*)(v120No_CapAddr1) = *(DWORD*)(&DamageCap);
+		double *DamageCap = new double(300000.00);
+		DWORD v120No_CapAddr1 = 0x00867A6C;
+		*(DWORD*)(v120No_CapAddr1) = *(DWORD*)(&DamageCap);
 	#endif
 }
