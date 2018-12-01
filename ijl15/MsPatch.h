bd746dad3a89aed720d314f7b6340034ba707319
reanox <hireanox@gmail.com>
2018-12-01 22:56:41 +0800

    增加v120所有傷害上限(物理攻擊、魔法攻擊、特殊技能、怪物傷害)

--- ijl15/MsPatch.h
+++ ijl15/MsPatch.h
@@ -8,6 +8,16 @@ void No_Cap() {
 	#elif MsVer == 120
 		double *DamageCap = new double(300000.00);
 		DWORD v120No_CapAddr1 = 0x00867A6C;
+		DWORD v120No_CapAddr2 = 0x00867CD6;
+		DWORD v120No_CapAddr3 = 0x00868D0E;
+		DWORD v120No_CapAddr4 = 0x00869D41;
+		DWORD v120No_CapAddr5 = 0x0086A213;
+		DWORD v120No_CapAddr6 = 0x0086A463;
 		*(DWORD*)(v120No_CapAddr1) = *(DWORD*)(&DamageCap);
+		*(DWORD*)(v120No_CapAddr2) = *(DWORD*)(&DamageCap);
+		*(DWORD*)(v120No_CapAddr3) = *(DWORD*)(&DamageCap);
+		*(DWORD*)(v120No_CapAddr4) = *(DWORD*)(&DamageCap);
+		*(DWORD*)(v120No_CapAddr5) = *(DWORD*)(&DamageCap);
+		*(DWORD*)(v120No_CapAddr6) = *(DWORD*)(&DamageCap);
 	#endif
 }
